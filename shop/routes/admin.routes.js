71092fd9846728075dc4e562fee29aee9f783391
kwoncha <rnjsckgus@gmail.com>
2023-10-25 13:19:45 +0900

    study

--- shop/routes/admin.routes.js
+++ shop/routes/admin.routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const adminController = require('../controllers/admin.controller');
+const imageUploadMiddleware = require('../middlewares/image-upload');
+
 const router = express.Router();
 
-router.get('/products',);
+router.get('/products', adminController.getProducts); // '/admin'이 앞에 있는것
 
-router.get('/products/new',);
+router.get('/products/new', adminController.getNewProduct);
+
+router.post('/products', imageUploadMiddleware, adminController.createNewProduct);
 
 module.exports = router;
